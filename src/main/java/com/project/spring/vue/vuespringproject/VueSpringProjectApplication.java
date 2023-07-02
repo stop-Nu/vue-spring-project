9fd20a7b80ef162d4ea90a83e01cc1ceb50d0178
stop-Nu <3rdchillisauce@naver.com>
2023-07-02 21:52:16 +0900

    initial commit

---
+++ src/main/java/com/project/spring/vue/vuespringproject/VueSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.spring.vue.vuespringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/spring/vue/vuespringproject/VueSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.spring.vue.vuespringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
