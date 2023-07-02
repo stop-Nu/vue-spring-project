c9707b73ab1f099c2f3bd0cd28c4be8540201a47
stop-Nu <3rdchillisauce@naver.com>
2023-07-02 22:38:24 +0900

    set openlayers

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,27 @@
+<!-- vue.js의 가장 초기화면 App.vue -->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id = "app"> 
+      <MainMap/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainMap from '@/components/MainMap'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default { 
+  name : 'App',
+  components : {
+      MainMap
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  position : absolute;
+  top : 0;
+  right : 0;
+  bottom : 0;
+  left : 0;
 }
 </style>
---
+++ frontend/src/components/MainMap.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class = "main-map" ref = "map">
+    </div>
+</template>
+
+<script>
+import OlLayerTile from 'ol/layer/Tile.js';
+import OlView from 'ol/View.js';
+import OlMap from 'ol/Map.js';
+import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj.js'
+
+export default {
+    name : 'MainMap',
+    data(){
+        return {
+            olMap:undefined,
+        }
+    },
+    mounted() {
+        this.olMap = new OlMap({
+            target : this.$refs.map,
+            layers:[
+                new OlLayerTile({
+                    source: new OSM()
+                })
+            ],
+            view : new OlView({
+                center : fromLonLat([126.8868688, 37.5730739]),
+                zoom : 10
+            })
+        })
+    },
+}
+</script>
+
+<style scoped>
+.main-map {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
